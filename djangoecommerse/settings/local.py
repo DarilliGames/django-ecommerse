528e3a687fd4e60807966f6e0f3f8a5e2b3db7f0
Stephen Moody <stephengmoody@hotmail.com>
2018-03-20 12:30:23 +0000

    Shoudld have access to thingy

--- djangoecommerse/settings/local.py
+++ djangoecommerse/settings/local.py
@@ -4,7 +4,7 @@ from .base import *
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
